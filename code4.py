9fa411c0515539117df28942b741bfe14415d969
John <thapajohn2057@gmail.com>
2024-04-19 07:15:44 +0545

    Scrooling website

--- code4.py
+++ code4.py
@@ -11,3 +11,22 @@ website_url= 'https://khalti.com/'
 # Open the website
 driver.get(website_url)
 driver.maximize_window()
+time.sleep(5)
+
+# Calculate the height of the webpage using javascript
+page_height = driver.execute_script("return document.body.scrollHeight")
+
+# Scroll down the webpage slowly using javascript
+scroll_speed = 100
+scroll_iterations = int(page_height / scroll_speed)
+
+# Loop to perform scrolling in increments
+for _ in range(scroll_iterations):
+    driver.execute_script(f"window.scrollBy(0, {scroll_speed});")
+    time.sleep(1)
+
+website_title= driver.title
+print(f"Website Title:{website_title}")
+
+driver.close()
+driver.quit()
